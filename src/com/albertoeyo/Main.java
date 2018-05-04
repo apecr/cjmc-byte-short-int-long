ace209b7e15619b2cfd6aec0c318a8073f1899c4
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-05-04 08:27:42 +0200

    doc(src/com/albertoeyo/Main.java) Challenge
    Documentation of the challenge
    
    [FA-38]

--- src/com/albertoeyo/Main.java
+++ src/com/albertoeyo/Main.java
@@ -3,25 +3,13 @@ package com.albertoeyo;
 public class Main {
 
     public static void main(String[] args) {
+        // 1. Create a byte variable and set it to any valid byte number.
+        // 2. Create a short variable and set it to any valid short number.
+        // 3. Create an int variable and set it to any valid int number.
+        // 4. Create a variable of type long, and make it equal to 5000 + 10
+        //    times the sum of the byte, plus the short plus the int
 
-        // int has a width of 32
-        int myMinValue = -2_147_483_648;
-        int myMaxValue = 2_147_483_647;
-        int myNewInt = myMinValue/2;
-        System.out.println("myNewInt = " + myNewInt);
+        
 
-        // byte has a width of 8
-        byte myByteValue = -128;
-        byte myNewByteValue = (byte) (myByteValue/2);
-        System.out.println("myNewByteValue = " + myNewByteValue);
-
-        // short has a width of 16
-        short myShortValue = 32767;
-        short myNewShortValue = (short) (myShortValue/2);
-        System.out.println("myNewShortValue = " + myNewShortValue);
-
-        // long has a width of 64
-        long myLongValue = 100L;
-        long minLongValue = -9_223_372_036_854_775_808L;
     }
 }
